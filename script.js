be075e7a45b78d01877af93a04263255dd092b27
c20246163..lucero@csav.edu.phgit config --global user.email git config --global user.name c20246163..lucero@csav.edu.phgit config --global user.name c20246163..lucero@csav.edu.ph <c20246163.lucero@csav.edu.ph>
2026-09-25 10:48:55 +0800

    Change MyProject bg to organge

---
+++ script.js
@@ -0,0 +1,68 @@
+console.log("ang ar lang ")
+const Myname = "Lucero, JohnKennth P.";
+console.log("name: " + Myname);
+let age = 22;
+let number = "09507718430";
+console.log("Age: " + age)
+console.log("Number: " + number);
+
+const address = "HDA.tison barangay 11 Victorias City, Neg";
+console.log("Address: " + address);
+
+function greet(name) {
+
+    return `good morning, ${name}`;
+
+
+}
+console.log(greet("rene"))
+
+const heading = document.querySelector("h1")
+console.log(heading);
+
+
+const contactHeading = document.querySelector("#contact h2");
+console.log(contactHeading);
+
+
+const servicesHeading = document.querySelector("#services h2");
+console.log(servicesHeading);
+
+const projectHeading = document.querySelector("#projects h2");
+console.log(projectHeading);
+
+
+
+
+//Text Context
+heading.textContent = "My portfolio";
+contactHeading.textContent = "Let's Connect!";
+projectHeading.textContent = "My Projects";
+
+heading.style.color = "purple";
+heading.style.backgroundColor = "yellow";
+contactHeading.style.color = "purple";
+projectHeading.style.color = "purple";
+servicesHeading.style.color = "purple";
+
+heading.addEventListener("click", function () {
+    heading.style.color = "red";
+});
+
+const toggleButton = document.querySelector('#switch');
+const body = document.querySelector('body');
+let isOff = false;
+
+toggleButton.addEventListener("click", function () {
+    isOff = !isOff;
+
+    if (isOff) {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+
+    }
+});
+
